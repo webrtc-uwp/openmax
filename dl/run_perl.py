c0ed89a5b9e646ab89fc8639e3a16343281acf21
Sergej Jovanovic <sergej@gnedo.com>
2018-04-24 17:54:41 +0200

    Openmax_dl_neon for ARM is built now from gn file

---
+++ dl/run_perl.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"This script is used to run a perl script."
+
+import optparse
+import subprocess
+import sys
+
+parser = optparse.OptionParser()
+parser.description = __doc__
+parser.add_option('-s', '--script', help='path to a perl script.')
+parser.add_option('-i', '--input', help='file passed to stdin.')
+parser.add_option('-o', '--output', help='file saved from stdout.')
+parser.add_option('-p', '--path', help='path')
+
+
+options, args = parser.parse_args()
+if (not options.script or not options.input or not options.output):
+  parser.error('Must specify arguments for script, input and output.')
+  sys.exit(1)
+
+pairs = open(options.path).read()[:-2].split('\0')
+kvs = [item.split('=', 1) for item in pairs]    
+with open(options.output, 'w') as fo, open(options.input, 'r') as fi:
+  subprocess.check_call(['perl','../../third_party/openmax_dl/dl/build/gas-preprocessor.pl', '-arch', 'arm', '-as-type', 'armasm', '-force-thumb', '--', 'armasm.exe', '-oldit', '-I../../third_party/openmax_dl/', options.input, '-o', options.output ], env=dict(kvs))
+
+sys.exit(0)
